63377d1ce8416b5dd8e9eb107704a61ad3e8d8a7
mahff <mahfoudamhiyen@live.fr>
2019-11-20 20:04:22 +0100

    header class : fuzz ndef header => to complete

---
+++ header.py
@@ -0,0 +1,25 @@
+
+
+
+
+class Header():
+
+    def __init__(self):
+        pass
+
+
+    def TNF(self): # RECORD TYPE
+        pass
+
+    def SR(self):
+        pass
+
+
+    def IL(self):
+        return 0 | 1
+
+    def ME(self):
+        pass
+
+    def URI_Identifier(self):
+        pass
